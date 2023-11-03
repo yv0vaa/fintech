3ac9fb55c2e11f13499ac40bd12ac16dfa33907d
yvovaa <zakharov.2004@yandex.ru>
2023-11-03 14:14:18 +0300

    solution commit. TODO: tests

--- src/main/java/edu/hw3/NullComparator.java
+++ src/main/java/edu/hw3/NullComparator.java
@@ -2,12 +2,12 @@ package edu.hw3;
 
 import java.util.Comparator;
 
-public class NullComparator implements Comparator<String>{
-    public int compare(String s1, String s2){
-        if (s1 == null){
+public class NullComparator implements Comparator<String> {
+    public int compare(String s1, String s2) {
+        if (s1 == null) {
             return -1;
         }
-        if (s2 == null){
+        if (s2 == null) {
             return 1;
         }
         return s1.compareTo(s2);
